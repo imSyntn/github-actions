767afb6bc3e2252015de391651daeecc471053c2
Sayantan Sarkar <imsyntn@gmail.com>
2026-02-17 14:51:30 +0530

    testing changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.status(200).json({ msg: "Hello from server!" })
+    return res.status(200).json({ msg: "Hello from server!", test: "Hey there!" })
 })
 
 app.listen(8080, () => { console.log("Server started.") })
